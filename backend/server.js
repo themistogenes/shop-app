7712dbbd4e79031e0f67911f09054038d7ba419a
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-09 21:40:55 -0400

    connect to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from "express"
+import { connectDB } from "./config/db.js"
 
 const app = express();
+
+// Routes
 
 app.get('/', (req, res) => {
   res.send('Server is ready');
 })
 
+app.get('/products', (req, res) => {
+  res.send('products');
+})
+
+// start server and connect to MongoDB
+
 app.listen(5000, () => {
+  connectDB()
   console.log('Server started at http://localhost:5000');
 })
