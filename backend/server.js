9d9e4541d98a4b27b80316b9df8a023983c6c515
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-09 23:12:02 -0400

    update product

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import { connectDB } from "./config/db.js"
+import mongoose from "mongoose"
 import Product from "./models/product.model.js";
 
 const app = express();
@@ -38,6 +39,24 @@ app.post('/api/products', async (req, res) => {
     res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
     console.error('Error creating product:', error.message);
+    res.status(500).json({ success: false, message: 'Server Error' });
+  }
+})
+
+// update product
+app.put('/api/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: 'Invalid product id' });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {new: true});
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    console.error('Error updating product:', error.message);
     res.status(500).json({ success: false, message: 'Server Error' });
   }
 })
