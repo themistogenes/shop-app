8a225ff82e37b5bad2210ab7c3662395d959eaff
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-10 13:05:26 -0400

    create routes in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-import { Button } from "@chakra-ui/react"
+import { Box, Button } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    </>
+    <Box minH={"100vh"}>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
   )
 }
 
