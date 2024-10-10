8c46aa5a9c72861b5b1a0fa1191b6c55a999dd2a
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-10 13:18:15 -0400

    create Navbar component; create HomePage and CreatePage pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-import { Box, Button } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CreatePage from "./pages/CreatePage"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <Box minH={"100vh"}>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
