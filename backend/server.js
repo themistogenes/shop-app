005080d6076926ffd2f6b3ccdf2e427f0eebe286
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-16 21:24:38 -0400

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from "path"
 import { connectDB } from "./config/db.js"
 import productRoutes from './routes/product.route.js';
 
@@ -8,15 +9,26 @@ dotenv.config(); // allow access to process.env
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 // Middleware
 app.use(express.json()); // allow JSON data in the req.body
 
 // Routes
 app.use('/api/products', productRoutes); // product routes
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-})
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// })
+
+// check for environment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  })
+}
 
 // start server and connect to MongoDB
 app.listen(PORT, () => {
